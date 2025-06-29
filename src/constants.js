2f996f81c1d348a823900b4c60a4b9a73b32f147
pratiksha0504 <142875466+pratiksha0504@users.noreply.github.com>
2025-06-29 20:24:47 +0530

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,249 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+import webverseLogo from './assets/company_logo/webverse_logo.png';
+import agcLogo from './assets/company_logo/agc_logo.png';
+import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+import prodigyLogo from './assets/company_logo/logo_prodigy.png';
+import SuvidhaLogo from './assets/company_logo/SuvidhaLogo.png';
+
+// Education Section Logo's
+import glaLogo from './assets/education_logo/gla_logo.png';
+import bsaLogo from './assets/education_logo/bsa_logo.png';
+import vpsLogo from './assets/education_logo/vps_logo.png';
+import pillai  from './assets/education_logo/pillai_logo.png';
+import XII  from './assets/education_logo/logo-college.png';
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/github_det.png';
+import csprepLogo from './assets/work_logo/cs_prep.png';
+import movierecLogo from './assets/work_logo/movie_rec.png';
+import taskremLogo from './assets/work_logo/task_rem.png';
+import npmLogo from './assets/work_logo/npm.png';
+import webverLogo from './assets/work_logo/web_dig.png';
+import webForever from './assets/work_logo/web_forever.png';
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+import closeBuy from './assets/work_logo/closebuy.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'BootStrap', logo: bootstrapLogo },
+     
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+    
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+    
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: prodigyLogo,
+      role: "Fullstack Developer",
+      company: "Prodigy Infotech",
+      date: "June 2025 - Present",
+      desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "TypeScript",
+        "Node JS",
+        "Tailwind CSS",
+        "MongoDb",
+        "Redux",
+        " Next Js",
+      ],
+    },
+    {
+      id: 1,
+      img: SuvidhaLogo,
+      role: "Web Development",
+      company: "Suvidha Foundation",
+      date: "Dec 2024 - Jan 2025",
+      desc: "In  this project I made Website for Suvidha Foundation.Suvidha Mahila Mandal, established on September 8, 1995, is a non-profit organization working to impart education among the financially challenged sections to help them realize parity in education and strength of little minds in building a promising future.",
+      skills: [
+        "ReactJS",
+        "JavaScript",
+        "Tailwind CSS",
+        "HTML",
+        "CSS",
+        "SQL",
+      ],
+    },
+    
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: pillai,
+      school: "Pillai College of Engineering, Panvel",
+      date: "Nov 2022 - may 2026",
+      grade: "9.6 CGPA",
+      desc: "I am a final year Electronics and Telecommunication (EXTC) engineering student at Pillai College of Engineering with a strong passion for coding and software development. I have built a strong foundation in Data Structures, Algorithms, OOPs, Web Development, and Database Systems. I’ve worked on several projects using C/C++, Python, JavaScript, Node.js, React, and MongoDB. My involvement in coding contests and tech events has sharpened my problem-solving skills and helped me grow as a developer.",
+
+      
+      degree: "Electronic and Telecommunication Engineer / BTech",
+    },
+    
+    {
+      id: 2,
+      img: XII,
+      school: "N N Paliwala Jr College, New Panvel",
+      date: "july 2020 - March 2022",
+      grade: "79.30%",
+      desc: "I completed my class 12 education from N N Paliwala Jr College , Panvel under the State board, where I studied Physics, Chemistry, and Mathematics (PCM) with Information Technology.",
+      degree: "XII / State Board - PCM with Information Technology",
+    },
+    {
+      id: 3,
+      img: XII,
+      school: "K A Bhanthiya High School, New Panvel",
+      date: "june 2011 - March 2020",
+      grade: "90.40%",
+      desc: "I completed my class 10 education from K A Bhanthiya High School, New Panvel, under the State board, where I studied Science with Computer.",
+      degree: "X, Science with Computer Application",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "FOREVER E-Commerce Website",
+      description:
+        "Developed a full-stack e-commerce website for Forever with dynamic product management, user authentication, and a secure shopping cart system. Implemented responsive frontend design and backend APIs for product listing, order processing, and user interactions.",
+      image: webForever,
+      tags: [ "Tailwind CSS", "JavaScript", "React JS", "API", "MongoDB", "Express", "Node JS"],
+      github: "https://github.com/pratiksha0504/PRODIGY_FS_Task3",
+      webapp: "https://forever-frontend1-sjkn.vercel.app/",
+    },
+    {
+      id: 1,
+      title: "CloseBuy",
+      description:
+        "Built a full-stack grocery shopping platform for CloseBuy, offering fresh vegetables, fruits, and daily essentials. Implemented user authentication, real-time product listings, and a responsive UI with a secure cart and order management system.",
+      image: closeBuy,
+      tags: ["React JS", "Node.js", "MongoDB", "Express", "HTML", "CSS", "JavaScript"],
+      github:"https://github.com/pratiksha0504/Closebuy",
+      webapp: "https://ecom-project-closebuy-frontend.vercel.app/",
+    },
+    {
+      id: 2,
+      title: "Movie Recommendation App",
+      description:
+        "A React-based web application that provides movie recommendations based on different criteria, such as genres, user preferences, and popular trends. The intuitive design and smooth experience make it a go-to app for movie enthusiasts.",
+      image: movierecLogo,
+      tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
+      
+      webapp: "https://movie-recommendation-app-jet.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "Email Validator NPM Package",
+      description:
+        "An efficient and customizable NPM package for validating email addresses. Built using React.js and Node.js, it provides robust validation features to help developers ensure that email inputs meet required formats and standards.",
+      image: npmLogo,
+      tags: ["React JS", "Node.js", "NPM", "Validation"],
+      
+      webapp: "https://www.npmjs.com/package/cmtk-email-validator",
+    },
+    {
+      id: 4,
+      title: "Task Reminder Chrome Extension Tool",
+      description:
+        "A productivity-boosting Chrome extension designed to help users manage and remember their daily tasks. Built using JavaScript, it offers a simple interface with reminders and task notifications to keep users on track.",
+      image: taskremLogo,
+      tags: ["JavaScript", "Chrome Extension", "HTML", "CSS"],
+      
+      webapp: "chrome://extensions/?id=kngheeibjnnidhfoomkpnbeghackheci",
+    },
+    {
+      id: 5,
+      title: "Webverse Digital",
+      description:
+        "The official website for Webverse Digital, a creative digital marketing agency. Built using HTML, CSS, and JavaScript, it features visually appealing animations and a clean design to showcase the agency's services.",
+      image: webverLogo,
+      tags: ["HTML", "CSS", "JavaScript", "Framer Motion"],
+     
+      webapp: "https://webversedigital.com/",
+    },
+    
+  ];  
