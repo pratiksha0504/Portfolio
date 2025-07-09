8a5275d14ec674e64b505d540534cd3bee101d0a
pratiksha0504 <chaudharipratiksha2005@gmail.com>
2025-07-09 20:48:11 +0530

    your commit message

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Navbar from "./Components/Navbar";
+import About from "./Components/About";
+import Skills from "./Components/Skills";
+import Experience from "./Components/Experience";
+import Work from "./Components/Work";
+import Education from "./Components/Education";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
+import BlurBlob from './Components/BlurBlob';
+
+
+
+function App() {
+
+  return (
+    <div className="bg-[#050414]">
+      
+      <BlurBlob position={{ top: '35%', left: '20%' }} size={{ width: '30%', height: '40%' }} />
+
+      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
+
+
+      <div className="relative pt-20">
+        <Navbar />
+        <About />
+        <Skills />
+        <Experience />
+        <Work />
+        <Education />
+        <Contact />
+        <Footer />
+      </div>
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default App
